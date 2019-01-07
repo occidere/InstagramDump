6c4abd20adfcba5b5cbce9481c7e5f767cd887cc
occidere <occidere@naver.com>
2019-01-07 20:58:39 +0900

    Convert to Spring boot based java config

---
+++ src/main/java/org/occidere/instagramdump/batch/Application.java
@@ -0,0 +1,12 @@
+package org.occidere.instagramdump.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class Application {
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Application.class, args);
+	}
+}
---
+++ src/main/java/org/occidere/instagramdump/batch/BatchConfiguration.java
@@ -0,0 +1,86 @@
+package org.occidere.instagramdump.batch;
+
+import org.occidere.instagramdump.common.InstagramCrawler;
+import org.occidere.instagramdump.domain.InstagramPhoto;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.*;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.support.transaction.ResourcelessTransactionManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableBatchProcessing
+public class BatchConfiguration extends DefaultBatchConfigurer {
+	@Override
+	public void setDataSource(DataSource dataSource) {
+	}
+
+	@Autowired
+	private JobBuilderFactory jobBuilderFactory;
+
+	@Autowired
+	private StepBuilderFactory stepBuilderFactory;
+
+	@Bean
+	public Job instagramDumpJob(Step instagramDumpStep) {
+		return jobBuilderFactory.get("instagramDumpJob")
+				.incrementer(new RunIdIncrementer())
+				.flow(instagramDumpStep)
+				.end()
+				.build();
+	}
+
+	@Bean
+	public Step instagramDumpStep() {
+		return stepBuilderFactory.get("instagramDumpStep")
+				.transactionManager(dummyTransactionManager())
+				.<InstagramPhoto, InstagramPhoto>chunk(1)
+				.reader(reader())
+				.processor(processor())
+				.writer(writer())
+				.build();
+	}
+
+	@Bean
+	public ResourcelessTransactionManager dummyTransactionManager() {
+		return new ResourcelessTransactionManager();
+	}
+
+	// TODO Boot web application 전환하여 REST API 로 배치 호출 가능하게 적용 
+	@Bean
+	@StepScope
+	public InstagramCrawler instagramCrawler(
+			@Value("#{jobParameters[url] == null ? 'https://www.instagram.com/wm_ohmygirl/?hl=ko' : jobParameters[url]}") String url,
+			@Value("#{jobParameters[dateRange] == null ? 1 : jobParameters[dateRange]}") int dateRange) {
+		InstagramCrawler crawler = new InstagramCrawler();
+		crawler.setUrl(url);
+		crawler.setRange(dateRange);
+		return crawler;
+	}
+
+	@Bean
+	public ItemReader<InstagramPhoto> reader() {
+		return new InstagramReader();
+	}
+
+	@Bean
+	public ItemProcessor<InstagramPhoto, InstagramPhoto> processor() {
+		return new InstagramProcessor();
+	}
+
+	@Bean
+	public ItemWriter<InstagramPhoto> writer() {
+		return new InstagramWriter();
+	}
+
+
+}
