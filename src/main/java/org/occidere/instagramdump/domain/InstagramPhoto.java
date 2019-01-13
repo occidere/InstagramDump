024c43175ba3ac2622a4c25539027d8fd813fcf4
occidere <occidere@naver.com>
2019-01-13 19:08:12 +0900

    id 변경, pageUrl 분리, date 에 formatter 적용

--- src/main/java/org/occidere/instagramdump/domain/InstagramPhoto.java
+++ src/main/java/org/occidere/instagramdump/domain/InstagramPhoto.java
@@ -1,10 +1,13 @@
 package org.occidere.instagramdump.domain;
 
 import lombok.Data;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 /**
@@ -17,11 +20,35 @@ import java.util.List;
 @Document(collection = "instagram")
 public class InstagramPhoto implements Serializable {
 	private static final long serialVersionUID = 1L;
+	private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
 
 	@Id
-	private String pageUrl;
+	private String id;
+	public void setId(String id) {
+		// https://www.instagram.com/p/BsS1PMbnw7B/ -> https://www.instagram.com/p/BsS1PMbnw7B
+		if (StringUtils.endsWith(id, "/")) {
+			id = id.substring(0, id.length() -1 );
+		}
+		// BsS1PMbnw7B
+		this.id = StringUtils.substringAfterLast(id, "/");
+	}
 
 	private String date; // yyyyMMddHHmmss
+	public void setDate(String date) {
+		try {
+			LocalDateTime.parse(date, FORMATTER);
+			this.date = date;
+		} catch (Exception e) {
+			this.date = LocalDateTime.now().format(FORMATTER);
+		}
+	}
+
+	private String pageUrl;
+	public void setPageUrl(String pageUrl) {
+		this.pageUrl = pageUrl;
+		setId(pageUrl);
+	}
+
 	private String author;
 	private String content;
 	private List<String> originImageUrls;
